1c2d239b70c6dd5848cc9931103e5df6b456d17a
brianbixby0@gmail.com <brianbixby0@gmail.com>
2016-12-06 09:46:37 -0800

    remove button works

--- js/script.js
+++ js/script.js
@@ -4,12 +4,11 @@ $(document).ready(function() {
   console.log("DOM is ready!");
 
   var toDoFormInput;
-  var param1;
+  $("ul").on("click", "li", removeItem);
 
   $("#submit").click(function() {
     toDoFormInput = $("#toDoFormInput").val();
     addToDoItem();
-    clearInput();
   });
 
   $("#wrapper").css("margin", "0 auto").css("width", "600px");
@@ -17,28 +16,19 @@ $(document).ready(function() {
   $("#enterName").css("height", "40px").css("color", "black");
   $("#plus").css("text-align", "right").css("float", "right").css("height", "40px");
   $("#plus2").css("margin-left", "20px").css("height", "40px");
+  $("#minus").css("height", "40px");
 
   $(".addToDo").click(function() {
     $("#hiding").toggle();
   });
 
   function addToDoItem() {
-    console.log("New to do iteam added");
-    $("#toDoListContainer").append("- " + toDoFormInput + "<br>");
+    $('#listOutput').append("<li>" + toDoFormInput + "</li>");
   }
 
-  //   function removeListItem() {
-  //     for (var j = 0; j < toDoListContainer.children.length; j++) {
-  //       toDoListContainer.children[j].addEventListener("click", listItemClicked);
-  //     }
-  //
-  //     function listItemClicked() {
-  //       if listItem[j] == this.id) {
-  //       $(document).on("dblclick", "toDoListContainer", function() {
-  //         $(toDoFormInput).fadeOut('slow');
-  //       });
-  //
-  //     }
-  //   }
-  // }
+  function removeItem() {
+    console.log('removing list item');
+    this.remove();
+  }
 });
+// $(li)
